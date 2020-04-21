bf9320c8b511b044d2bc0c52e6ed0a3be98ced9f
Gabriel Henrique <gabrielhenrique_3l@outlook.com>
2020-04-20 23:25:57 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+import api from "./services/api";
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    async function loadRepositories() {
+      const response = await api.get("repositories");
+      setRepositories(response.data);
+    }
+    loadRepositories();
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("repositories", {
+      title: `Desafio Node.js ${Date.now()}`,
+      url: "https://github.com/GabrielHenriqueS/omnistack11-frontend",
+      techs: ["Node.js", "ReactJS"],
+      likes: 10
+    });
+
+    const { data } = response;
+
+    setRepositories([...repositories, data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    const data = repositories.filter(repository => repository.id !== id);
+
+    setRepositories(data);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
